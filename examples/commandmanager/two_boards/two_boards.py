798843041d7918c506bd36808fd41e1fca6e9e15
jgrizou <jonathan.grizou@glasgow.ac.uk>
2015-12-07 13:21:21 +0000

    made axis and mutliaxis device converting steps to units

--- examples/commandmanager/two_boards/two_boards.py
+++ examples/commandmanager/two_boards/two_boards.py
@@ -9,13 +9,6 @@ s1 = cmdMng.devices['servo1']
 s2 = cmdMng.devices['servo2']
 m1 = cmdMng.devices['stepper1']
 
-# quit = False
-# while not quit:
-#     msg = raw_input()
-#     if msg == "QUIT":
-#         isRunning = False
-#         quit = True
-#     elif msg == "REQUEST":
-#         print(cmdMng.devices['servo1'].get_angle())
-#     else:
-#         cmdMng.serialcommandhandlers[0].write(msg)
+from commandruino.devices.axis import Axis
+
+a = Axis(m1, 0.1)
