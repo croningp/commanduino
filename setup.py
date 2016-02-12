45cc9d5e5797e05a9134a288429919aa6bc62c58
jgrizou <jonathan.grizou@glasgow.ac.uk>
2016-02-12 11:09:32 +0000

    update setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
-setup(name="commandruino",
+setup(name="commanduino",
       version="0.1",
       description="A library to interact with an arduino board running Arduino-CommandHandler and derivatives",
       author="Jonathan Grizou",
       author_email='jonathan.grizou@gmail.com',
       platforms=["any"],
-      url="https://github.com/croningp/commandruino",
+      url="https://github.com/croningp/commanduino",
       packages=find_packages(),
       )
