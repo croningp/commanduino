8c93b91975bb8cb92deee76b1bf6474bad262b72
jgrizou <jonathan.grizou@glasgow.ac.uk>
2016-02-17 16:20:39 +0000

    modified servo to comply with register_request()

--- commanduino/commanddevices/commandservo.py
+++ commanduino/commanddevices/commandservo.py
@@ -16,27 +16,20 @@ class CommandServo(CommandDevice):
 
     def __init__(self):
         CommandDevice.__init__(self)
-
-        # angle
-        self.angle = 0
-        self.angle_lock = Lock()
-        self.cmdHdl.add_command(CMD_ANSWER_ANGLE, self.handle_angle_command)
+        self.register_all_requests()
 
     ##
-    def request_angle(self):
-        self.send(CMD_REQUEST_ANGLE)
+    def set_angle(self, angle):
+        self.send(CMD_SET_ANGLE, int(angle))
+
+    def register_all_requests(self):
+        self.register_request(
+            CMD_REQUEST_ANGLE,
+            CMD_ANSWER_ANGLE,
+            'angle',
+            self.handle_angle_command)
 
     def handle_angle_command(self, *arg):
         if arg[0]:
             self.angle = int(arg[0])
             self.angle_lock.ensure_released()
-
-    def set_angle(self, angle):
-        self.send(CMD_SET_ANGLE, int(angle))
-
-    def get_angle(self):
-        self.angle_lock.acquire()
-        self.request_angle()
-        is_valid, elapsed = self.angle_lock.wait_until_released()
-        self.angle_lock.ensure_released()
-        return self.angle, is_valid, elapsed
