d200d5579da946cd077d6d33e69c5b6adb09cee3
jgrizou <jonathan.grizou@glasgow.ac.uk>
2016-05-30 11:07:06 +0100

    version handled automatically by setup

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
+from commanduino._version import __version__
+
 from setuptools import find_packages, setup
 setup(name="commanduino",
-      version="0.1",
+      version=__version__,
       description="A library to interact with an arduino board running Arduino-CommandHandler and derivatives",
       author="Jonathan Grizou",
       author_email='jonathan.grizou@gmail.com',
