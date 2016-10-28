f3f7bc47a64e4ca14e7f5ba5bcf6e70c9bc3be47
jgrizou <jonathan.grizou@glasgow.ac.uk>
2016-10-28 18:59:38 +0100

    added demo file showing some issues, probably coming from the arduino side but to be investigated

---
+++ examples/commanddevices/commandaccelstepper/demo.py
@@ -0,0 +1,52 @@
+import time
+import logging
+logging.basicConfig(level=logging.INFO)
+
+
+from commanduino import CommandManager
+
+cmdMng = CommandManager.from_configfile('./demo.json')
+
+
+stepper = cmdMng.stepper
+
+stepper.set_current_position(0)
+
+stepper.enable_acceleration()
+stepper.set_acceleration(1000)
+stepper.set_max_speed(10000)
+
+print('Moving to 20000 with acceleration 1000')
+stepper.move_to(20000, wait=True)
+
+print('Moving back to 0 with acceleration 5000...')
+print('Should turn in the oposite direction no?')
+stepper.set_acceleration(5000)
+stepper.move_to(0, wait=True)
+
+#
+print('Disabling acceleration')
+stepper.disable_acceleration()
+stepper.set_max_speed(10000)
+
+#
+print('Moving to 20000 with default speed 1000')
+stepper.move_to(20000, wait=False)
+time.sleep(1)
+print('Increasing speed to 10000')
+stepper.set_speed(10000)
+stepper.wait_until_idle()
+
+#
+print('Moving back to 0')
+print('Should turn in the oposite direction no?')
+print('Speed should still be 10000 no?')
+stepper.move_to(0, wait=True)
+
+
+print('Current position is {}'.format(stepper.get_current_position()))
+print('Current position is good but the motor always moved in the same direction...')
+
+#
+print('get_target_position() does not work')
+stepper.get_target_position()
