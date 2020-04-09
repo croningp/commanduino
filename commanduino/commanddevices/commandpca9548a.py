ee610df2a633012457a583088dc6bd833cc442ca
Sergey Zalesskiy <s.zalesskiy@gmail.com>
2020-04-09 17:35:01 +0300

    * Reverted to passing simply channels mask instead of variable arguments list

--- commanduino/commanddevices/commandpca9548a.py
+++ commanduino/commanddevices/commandpca9548a.py
@@ -58,32 +58,17 @@ class CommandPCA9548A(CommandDevice):
             *arg: Variable command.
         """
         if arg[0]:
-            # Get active channels mask as an int
-            # And convert to eight-bit padded binary string("0b00000000")
-            channels_mask = format(int(arg[0]), "#010b")
-            # Chop off "0b" prefix
-            channels_mask = channels_mask[2:]
-            # Split to list of ints
-            self.channels = [int(c) for c in channels_mask]
+            # This would be a one byte representing active channels mask
+            self.channels = int(arg[0])
             self.channels_lock.ensure_released()
 
-    def set_channels(self, *channels):
+    def set_channels(self, channels):
         """Sets the state of all I2C channels on the MUX.
-        If an arguments is not supplied for the channel, it would be switched off.
 
         Args:
-            *channels: List of integer channel states (e.g [1,0,0,1]) where index represents
-            channel number, 0/1 - channel state.
+            channels: An integer representing an 8-bit long mask of channels to switch on/off.
+            Anything bigger than 2^8=255 is set to zero on Arduino side anyway.
         """
-        # Map every channel state to 0 or 1
-        command_args = []
-        for value in channels:
-            if value == 0 or value == 1:
-                command_args.append(value)
-            else:
-                command_args.append(0)
-        # Chop to 8 channels
-        # This is actually checked on the Arduino side, so just not to send
-        # unnecessary long strings
-        command_args = command_args[:MAX_CHANNELS]
-        self.send(CMD_SET_STATE, *command_args)
+        if channels > 255:
+            channels = 0
+        self.send(CMD_SET_STATE, channels)
