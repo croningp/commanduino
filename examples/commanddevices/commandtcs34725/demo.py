b5f752ba1697a8383a491df93756ef9881bda1ff
ArtemLeonov <artem.leonov@glasgow.ac.uk>
2020-08-17 14:47:34 +0100

    Added example to deal with initialization code

--- examples/commanddevices/commandtcs34725/demo.py
+++ examples/commanddevices/commandtcs34725/demo.py
@@ -1,3 +1,7 @@
+"""
+An example module to work with the TCS34725 RGB sensor as Arduino device.
+"""
+
 import time
 import logging
 import os
@@ -10,9 +14,21 @@ DEMO = os.path.join(HERE, 'demo.json')
 
 cmdMng = CommandManager.from_configfile(DEMO)
 
-# print(cmdMng.__dict__)
+# Upon instantiation, initialization code can be used to check if the sensor is actually attached
+# <sensor>.initialization_code is None - the device wasn't initialized, call get_initialization_code()
+# <sensor>.initialization_code == 1 - the device is found and the communication was established successfully
+# <sensor>.initialization_code == 0 - the device was not found or the communication cannot be established, please
+#   check the device datasheet for possible reasons
+#
+# Call get_initialization_code() to try again.
 
-for i in range(10):
+# Below is an example code for a check
+class SensorError(Exception):
+    """ Generic sensor error. """
+if cmdMng.demo.initialization_code is not None and cmdMng.demo.initialization_code == 0:
+    raise SensorError("Sensor is not connected!")
+
+for i in range(3):
     start_time = time.time()
     C = cmdMng.demo.get_rgbc()
     end_time = time.time()
@@ -20,7 +36,7 @@ for i in range(10):
 
 print('INTEGRATION TIME 700')
 cmdMng.demo.set_integration_time(700)
-for i in range(10):
+for i in range(3):
     start_time = time.time()
     C = cmdMng.demo.get_rgbc()
     end_time = time.time()
@@ -28,7 +44,7 @@ for i in range(10):
 
 print('GAIN 16')
 cmdMng.demo.set_gain(16)
-for i in range(10):
+for i in range(3):
     start_time = time.time()
     C = cmdMng.demo.get_rgbc()
     end_time = time.time()
